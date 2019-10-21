d30b543012ac9e084cb5c0e251f0445da95cebf1
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-10-20 21:24:01 -0700

    alternate solution with list comprehension that sadly is longer

--- divisors.py
+++ divisors.py
@@ -4,3 +4,10 @@ for i in range(1,101):
  for j in range(1,i+1):
   if i%j==0:print(j,end=' ')
  print()
+
+
+# attempt at alternate solution using a nested list comprehension
+# sadly it is 110 chars, way longer than the 84 chars above
+# looked into translate() and r=str.replace but made no headway
+
+print(str([y for x in range(1,101)for y in range(1,x+1)if x%y==0]).replace(' 1,','\n1').replace(',','')[1:-1])
